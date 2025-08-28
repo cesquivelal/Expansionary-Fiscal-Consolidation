5bfa9050490340bb2910e425d9597a7202278a8a
cesquivelal <56401749+cesquivelal@users.noreply.github.com>
2025-08-28 16:06:20 -0400

    Update mainAll.jl

--- Model/mainAll.jl
+++ Model/mainAll.jl
@@ -1,33 +1,17 @@
 
 using Distributed
-addprocs(39)
+addprocs(62)
 
 @everywhere include("Primitives.jl")
 
-@everywhere XX=readdlm("Setup.csv",',')
-@everywhere VEC_PAR=XX[2:end,2]*1.0
-@everywhere par, GRIDS=Setup_From_Vector(VEC_PAR)
-
 ###############################################################################
-#Solve benchmark decentralized
+#Try different debt limits, benchmark
 ###############################################################################
-@everywhere NAME_DEC="SOL_DEC.csv"
-Decentralized=true
-SolveAndSaveModel(Decentralized,NAME_DEC,GRIDS,par)
-
-###############################################################################
-#Try different debt limits
-###############################################################################
-@everywhere N_FR=39
-@everywhere fr_low=0.35
-@everywhere fr_high=0.60
-@everywhere grFR=collect(range(fr_low,stop=fr_high,length=N_FR))
-
-@everywhere MOD_DEC=UnpackModel_File(NAME_DEC," ")
-FR=TestManyFiscalRules_Dec(grFR,MOD_DEC)
-
-@everywhere N_χ=39
-@everywhere χ_low=0.00
-@everywhere χ_high=0.20
-@everywhere grχ=collect(range(χ_low,stop=χ_high,length=N_χ))
-TestManyTransitionSmoothers_Dec(FR,grχ,MOD_DEC)
+@everywhere SETUP_FILE="Setup.csv"
+DoPairs=true; IsFirst=true
+TestAll_DebtLimits_And_FiscalRules(DoPairs,IsFirst,2,SETUP_FILE)
+IsFirst=false
+TestAll_DebtLimits_And_FiscalRules(DoPairs,IsFirst,3,SETUP_FILE)
+TestAll_DebtLimits_And_FiscalRules(DoPairs,IsFirst,4,SETUP_FILE)
+TestAll_DebtLimits_And_FiscalRules(DoPairs,IsFirst,5,SETUP_FILE)
+TestAll_DebtLimits_And_FiscalRules(DoPairs,IsFirst,6,SETUP_FILE)
