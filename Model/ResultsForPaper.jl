371a83527ad20e375099577ba94462232447e4ad
cesquivelal <56401749+cesquivelal@users.noreply.github.com>
2025-08-28 16:07:56 -0400

    Update ResultsForPaper.jl

--- Model/ResultsForPaper.jl
+++ Model/ResultsForPaper.jl
@@ -1,70 +1,29 @@
 
-using Distributed, BenchmarkTools
-using Plots; pyplot(fontfamily="serif",linewidth=2.0,grid=false,legend=true,
-                    background_color_legend=nothing,foreground_color_legend=nothing,
-                    legendfontsize=20,guidefontsize=20,titlefontsize=20,tickfontsize=20,
-                    markersize=9,size=(650,500))
+using Distributed
 
-################################################################
-######## Load all functions and files with solutions ###########
-################################################################
-#Load functions
 include("Primitives.jl")
 include("AnalysisFunctions.jl")
 
-#Load benchmark model
 FOLDER="AllExercises"
-NAME_DEC="SOL_DEC.csv"
-MOD=UnpackModel_File(NAME_DEC,FOLDER)
+FOLDER_GRAPHS="Graphs"
+######################################################
+######### Load benchmark model #######################
+######################################################
+case_coulumn=2
+MOD, MOD_DL, MOD_DefL, MOD_FR=Unpack_All_Models_Case(case_coulumn,FOLDER)
 
-#Load benchmark model with debt limit and consolidation plan
-FOLDER_FILE_WG="$FOLDER\\WelfareGains_DEC.csv"
-FOLDER_FILE_WG_TR="$FOLDER\\WelfareGainsTransition_DEC.csv"
-
-ARR_SOLS_DEC=readdlm("$FOLDER\\ALL_SOLS_DEC_FR_0.csv",',')
-ARR_SOLS_DEC_TR=readdlm("$FOLDER\\ALL_SOLS_DEC_FR_CHI.csv",',')
-
-XX=readdlm(FOLDER_FILE_WG,',')
-GAINS=1.0*XX[2:end,2]
-wg, i_fr=findmax(GAINS[:,1])
-MOD_FR=UnpackModel_Vector(ARR_SOLS_DEC[:,i_fr])
-
-XX=readdlm(FOLDER_FILE_WG_TR,',')
-GAINS=1.0*XX[2:end,2]
-wg, i_χ=findmax(GAINS[:,1])
-MOD_FR_TR=UnpackModel_Vector(ARR_SOLS_DEC_TR[:,i_χ])
-
-#Load models with no commitment, lax and strict
-FOLDER_GRIM="GrimStrategy"
-NAME_GRIM="SOL_GRIM_Lax.csv"
-MOD_GRIM_Lax=UnpackModel_File(NAME_GRIM,FOLDER_GRIM)
-
-NAME_GRIM="SOL_GRIM_Strict.csv"
-MOD_GRIM_Strict=UnpackModel_File(NAME_GRIM,FOLDER_GRIM)
-
-################################################################
-################# Abstract: long-run changes ###################
-################################################################
-ErgodicStart=true; by0=0.46
-NoDefaults=false; ForSlides=false
-N=20000; Tbefore=4; Tafter=100
-TS_Av, TS_FR_Av=Average_Consolidation_Paths(NoDefaults,ErgodicStart,by0,N,Tbefore,Tafter,MOD,MOD_FR)
-100*((minimum(TS_FR_Av.Cons)/TS_FR_Av.Cons[1])-1)
-100*((minimum(TS_FR_Av.Inv)/TS_FR_Av.Inv[1])-1)
-100*((TS_FR_Av.GDP[end]/TS_FR_Av.GDP[1])-1)
-
-################################################################
-################# Table 2: Targeted moments ####################
-################################################################
-Tmom=1000; NSamplesMoments=100
+######################################################
+######### Table 2 - Targeted moments #################
+######################################################
+Tmom=1000; NSamplesMoments=10000
 MOM=AverageMomentsManySamples(Tmom,NSamplesMoments,MOD)
 MOM.σ_inv/MOM.σ_GDP
 MOM.MeanSpreads
 MOM.Debt_GDP
 
-################################################################
-################ Table 3: Non-targeted moments #################
-################################################################
+######################################################
+######### Table 3 - Untargeted moments ###############
+######################################################
 MOM.DefaultPr
 MOM.StdSpreads
 MOM.σ_con/MOM.σ_GDP
@@ -73,119 +32,173 @@ MOM.σ_TB_y
 MOM.Corr_Spreads_GDP
 MOM.Corr_TB_GDP
 
-################################################################
-################ Figure 1: Optimal debt limit ##################
-################################################################
-DeficitLimit=false
-plt=Plot_WelfareGains(DeficitLimit,FOLDER_FILE_WG)
-savefig(plt,"Graphs\\WG_DebtLimit.pdf")
+######################################################
+######### Figure 1 and 3 - Welfare gains #############
+######################################################
+case_coulumn=2
+fig_1, fig_3, fig_1App=All_WelfareGains_Plots(case_coulumn,FOLDER)
+fig_1
+savefig(fig_1,"$FOLDER_GRAPHS\\Figure_1.pdf")
+fig_3
+savefig(fig_3,"$FOLDER_GRAPHS\\Figure_3.pdf")
+fig_1App
+savefig(fig_1App,"$FOLDER_GRAPHS\\Figure_1App.pdf")
 
-################################################################
-####### Figure 2: Expansionary fiscal consolidation ############
-################################################################
-ErgodicStart=true; by0=0.46
-NoDefaults=false; ForSlides=false
-N=20000; Tbefore=4; Tafter=20
-pltEFC=Plot_OneConsolidationPath(ForSlides,NoDefaults,ErgodicStart,by0,N,Tbefore,Tafter,MOD,MOD_FR)
-if ForSlides
-    NAME="Graphs\\EFC_slides.pdf"
-else
-    NAME="Graphs\\EFC.pdf"
-end
-savefig(pltEFC,NAME)
+wg_dl, dl_star, wg_dr, dl_dr_star, χ_dr_star, wg_bbr, bbr_star=Optimal_Fiscal_Rules(case_coulumn,FOLDER)
+wg_dl
+wg_dr
+wg_bbr
 
-################################################################
-########## Figure 3: Spreads and the price of debt #############
-################################################################
-plt=PlotPriceAndSpreads(N,Tbefore,Tafter,MOD,MOD_FR)
-NAME="Graphs\\PriceSchedule.pdf"
-savefig(plt,NAME)
+dl_star
 
-################################################################
-############# Figure 4: Optimal consolidation plan #############
-################################################################
-DeficitLimit=true
-plt=Plot_WelfareGains(DeficitLimit,FOLDER_FILE_WG_TR)
-savefig(plt,"Graphs\\WG_DeficitLimitTransition.pdf")
+dl_dr_star
+χ_dr_star
 
-################################################################
-#### Figure 4: Expansionary fiscal consolidation, long-run #####
-################################################################
-Tafter=80
-pltEFC=Plot_OneConsolidationPath(ForSlides,NoDefaults,ErgodicStart,by0,N,Tbefore,Tafter,MOD,MOD_FR)
-if ForSlides
-    NAME="Graphs\\EFC_LR_slides.pdf"
-else
-    NAME="Graphs\\EFC_LR.pdf"
-end
-savefig(pltEFC,NAME)
+bbr_star
 
-################################################################
-############## Table 4: Moments different cases ################
-################################################################
-MOM_FR_TR=AverageMomentsManySamples(Tmom,NSamplesMoments,MOD_FR_TR)
-MOM_GRIM_Strict=AverageMomentsManySamples(Tmom,NSamplesMoments,MOD_GRIM_Strict)
+######################################################
+###### Figure 2 - Transition to fiscal rule ##########
+######################################################
+case_coulumn=2
+N=20000
+Tafter=40
+fig_2, fig_2App=Plot_Consolidations_Case_Bav_And_B0(N,Tafter,case_coulumn,FOLDER)
+fig_2
+savefig(fig_2,"$FOLDER_GRAPHS\\Figure_2.pdf")
+fig_2App
+savefig(fig_2App,"$FOLDER_GRAPHS\\Figure_2App.pdf")
+
+Tafter=160
+fig_2_lr, fig_2App_lr=Plot_Consolidations_Case_Bav_And_B0(N,Tafter,case_coulumn,FOLDER)
+fig_2_lr
+savefig(fig_2_lr,"$FOLDER_GRAPHS\\Figure_2_lr.pdf")
+fig_2App_lr
+savefig(fig_2App_lr,"$FOLDER_GRAPHS\\Figure_2App_lr.pdf")
+
+######################################################
+###### Figure 4 - Spreads and price of debt ##########
+######################################################
+fig_4, fig_4App=PlotPriceAndSpreads_Case(case_coulumn,FOLDER)
+fig_4
+savefig(fig_4,"$FOLDER_GRAPHS\\Figure_4.pdf")
+fig_4App
+savefig(fig_4App,"$FOLDER_GRAPHS\\Figure_4App.pdf")
+
+######################################################
+######### Table 4 - Compare optimal rules ############
+######################################################
+#Benchmark case is case_column=2, foreign investors is 4, patient hh is 5
+case_coulumn=5
+MOD, MOD_DL, MOD_DefL, MOD_FR=Unpack_All_Models_Case(case_coulumn,FOLDER)
+
+wg_dl, dl_star, wg_dr, dl_dr_star, χ_dr_star, wg_bbr, bbr_star=Optimal_Fiscal_Rules(case_coulumn,FOLDER)
+dl_star
+dl_dr_star
+χ_dr_star
+bbr_star
+
+wg_dl
+wg_dr
+wg_bbr
+
+MOM=AverageMomentsManySamples(Tmom,NSamplesMoments,MOD)
+MOM_DL=AverageMomentsManySamples(Tmom,NSamplesMoments,MOD_DL)
+MOM_FR=AverageMomentsManySamples(Tmom,NSamplesMoments,MOD_FR)
+MOM_BBR=AverageMomentsManySamples(Tmom,NSamplesMoments,MOD_DefL)
 
 MOM.DefaultPr
-MOM_FR_TR.DefaultPr
-MOM_GRIM_Strict.DefaultPr
+MOM_DL.DefaultPr
+MOM_FR.DefaultPr
+MOM_BBR.DefaultPr
 
 MOM.MeanSpreads
-MOM_FR_TR.MeanSpreads
-MOM_GRIM_Strict.MeanSpreads
+MOM_DL.MeanSpreads
+MOM_FR.MeanSpreads
+MOM_BBR.MeanSpreads
 
 MOM.StdSpreads
-MOM_FR_TR.StdSpreads
-MOM_GRIM_Strict.StdSpreads
+MOM_DL.StdSpreads
+MOM_FR.StdSpreads
+MOM_BBR.StdSpreads
 
 MOM.Debt_GDP
-MOM_FR_TR.Debt_GDP
-MOM_GRIM_Strict.Debt_GDP
-
-T=20000
-TS_BEN=Simulate_Paths_Ergodic(T,MOD)
-TS_FR_TR=Simulate_Paths_Ergodic(T,MOD_FR_TR)
-TS_GRIM_STR=Simulate_Paths_Ergodic(T,MOD_GRIM_Strict)
-
-100*(mean(TS_FR_TR.Cons ./ TS_BEN.Cons)-1)
-
-μc_ben=mean(TS_BEN.Cons)
-μc_fr_tr=mean(TS_FR_TR.Cons)
-μc_str=mean(TS_GRIM_STR.Cons)
-
-μc_fr_tr/μc_ben
-μc_str/μc_ben
+MOM_DL.Debt_GDP
+MOM_FR.Debt_GDP
+MOM_BBR.Debt_GDP
 
 MOM.σ_con/MOM.σ_GDP
-MOM_FR_TR.σ_con/MOM_FR_TR.σ_GDP
-MOM_GRIM_Strict.σ_con/MOM_GRIM_Strict.σ_GDP
+MOM_DL.σ_con/MOM_DL.σ_GDP
+MOM_FR.σ_con/MOM_FR.σ_GDP
+MOM_BBR.σ_con/MOM_FR.σ_GDP
 
-ZeroDebt=false; Nwg=10000
-wg_ben=AverageWelfareGains(ZeroDebt,Nwg,MOD,MOD_FR_TR)
-wg_str=AverageWelfareGains(ZeroDebt,Nwg,MOD,MOD_GRIM_Strict)
+T=1000
+TS_no_rule=Simulate_Paths_Ergodic(T,MOD)
+TS_DL=Simulate_Paths_Ergodic(T,MOD_DL)
+TS_FR=Simulate_Paths_Ergodic(T,MOD_FR)
+TS_BBR=Simulate_Paths_Ergodic(T,MOD_DefL)
+mean(TS_DL.Cons ./ TS_no_rule.Cons)
+mean(TS_FR.Cons ./ TS_no_rule.Cons)
+mean(TS_BBR.Cons ./ TS_no_rule.Cons)
 
-################################################################
-#### Figure 6: Expansionary fiscal consolidation, with plan ####
-################################################################
-Tafter=20; WithCommitment=true
-pltEFC=Plot_TwoConsolidationPaths(WithCommitment,ForSlides,ErgodicStart,by0,N,Tbefore,Tafter,MOD,MOD_FR,MOD_FR_TR)
-if ForSlides
-    NAME="Graphs\\EFC_OptTran_slides.pdf"
-else
-    NAME="Graphs\\EFC_OptTran.pdf"
-end
-savefig(pltEFC,NAME)
+#############################################################
+### Figure 5 - Transition to fiscal rule, foreign capital ###
+#############################################################
+case_coulumn=4
+Tafter=40
+fig_5, fig_5App=Plot_Consolidations_Case_Bav_And_B0(N,Tafter,case_coulumn,FOLDER)
+fig_5
+savefig(fig_5,"$FOLDER_GRAPHS\\Figure_5.pdf")
+fig_5App
+savefig(fig_5App,"$FOLDER_GRAPHS\\Figure_5App.pdf")
 
+Tafter=160
+fig_5_lr, fig_5App_lr=Plot_Consolidations_Case_Bav_And_B0(N,Tafter,case_coulumn,FOLDER)
+fig_5_lr
+savefig(fig_5_lr,"$FOLDER_GRAPHS\\Figure_5_lr.pdf")
+fig_5App_lr
+savefig(fig_5App_lr,"$FOLDER_GRAPHS\\Figure_5App_lr.pdf")
 
+case_coulumn=5
+Tafter=40
+fig_5hh, fig_5hhApp=Plot_Consolidations_Case_Bav_And_B0(N,Tafter,case_coulumn,FOLDER)
+fig_5hh
+savefig(fig_5hh,"$FOLDER_GRAPHS\\Figure_5hh.pdf")
+fig_5hhApp
+savefig(fig_5hhApp,"$FOLDER_GRAPHS\\Figure_5hhApp.pdf")
 
-################################################################
-### Figure 7: Expansionary fiscal consolidation, strict grim ###
-################################################################
-WithCommitment=false
-pltEFC=Plot_TwoConsolidationPaths(WithCommitment,ForSlides,ErgodicStart,by0,N,Tbefore,Tafter,MOD,MOD_FR_TR,MOD_GRIM_Strict)
-if ForSlides
-    NAME="Graphs\\EFC_NoCommitment_slides.pdf"
-else
-    NAME="Graphs\\EFC_NoCommitment.pdf"
-end
-savefig(pltEFC,NAME)
+Tafter=160
+fig_5hh_lr, fig_5hhApp_lr=Plot_Consolidations_Case_Bav_And_B0(N,Tafter,case_coulumn,FOLDER)
+fig_5hh_lr
+savefig(fig_5hh_lr,"$FOLDER_GRAPHS\\Figure_5hh_lr.pdf")
+fig_5hhApp_lr
+savefig(fig_5hhApp_lr,"$FOLDER_GRAPHS\\Figure_5hhApp_lr.pdf")
+
+######################################################
+############### Welfare decomposition ################
+######################################################
+MOD_Benchmark, MOD_Planner, MOD_Covenants, MOD_Efficient=Models_Welfare_Decomposition(FOLDER)
+
+N=10000
+#Average welfare gains of first-best
+av_wg_fb=AverageWelfareGains(N,MOD_Benchmark,MOD_Efficient)
+
+#Welfare gains of first-best from average state
+#for welfare decomposition
+wg_Ben_Eff=WelfareGains_AvState(N,MOD_Benchmark,MOD_Benchmark,MOD_Efficient)
+
+#First decomposition
+#benchmark -> covenants -> centralized investment
+wg_Ben_Cov=WelfareGains_AvState(N,MOD_Benchmark,MOD_Benchmark,MOD_Covenants)
+wg_Cov_Eff=WelfareGains_AvState(N,MOD_Benchmark,MOD_Covenants,MOD_Efficient)
+
+#Second decomposition
+#benchmark -> centralized investment -> covenants
+wg_Ben_Pla=WelfareGains_AvState(N,MOD_Benchmark,MOD_Benchmark,MOD_Planner)
+wg_Pla_Eff=WelfareGains_AvState(N,MOD_Benchmark,MOD_Planner,MOD_Efficient)
+
+#Average welfare gains from 0 debt
+#Footnote 15
+case_coulumn=2
+MOD, MOD_DL, MOD_DefL, MOD_FR=Unpack_All_Models_Case(case_coulumn,FOLDER)
+wg=AverageWelfareGains_0(N,MOD_Benchmark,MOD_DefL)
+wg=AverageWelfareGains_0(N,MOD_Benchmark,MOD_Efficient)
